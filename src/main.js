5b1b240e6b8d4514a0b0a7f055d5fa4a11c2c2f8
Brian <brianyoo313@gmail.com>
2017-09-20 23:46:04 -0400

    adjustments to scrollmagic navigation

--- src/main.js
+++ src/main.js
@@ -5,3 +5,6 @@ require('./sass/style.scss');
 // JAVASCRIPT
 require('./js/navigation.js');
 require('./js/google-map.js');
+
+require('./js/TweenLite.min.js');
+require('scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js');
